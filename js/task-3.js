37fe0c06caa24f33fce8523836f843e077ec7a21
aysegularslan <arslanaysegull@outlook.com>
2025-05-04 21:51:54 +0300

    js-07 revize

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,7 @@
-const input = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
-function replace() {
-  if (input.value) {
-    output.textContent = input.value;
-  } else {
-    output.textContent = 'Anonymous';
-  }
-}
-
-input.addEventListener('input', replace);
+input.addEventListener('input', () => {
+  const trimmedValue = input.value.trim();
+  output.textContent = trimmedValue ? trimmedValue : 'Anonymous';
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,20 @@
-const form = document.querySelector('form.login-form');
+const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  let formData = {};
+  const inputs = form.querySelectorAll('input');
+  const formData = {};
 
-  for (const element of form.elements) {
-    if (element.tagName === 'INPUT') {
-      if (element.value) {
-        formData[element.name] = element.value;
-      } else {
-        alert('All form fields must be filled in');
-        formData = {};
-      }
+  for (const input of inputs) {
+    const { name, value } = input;
+
+    if (value.trim() === '') {
+      alert('All form fields must be filled in');
+      return;
     }
+
+    formData[name] = value.trim();
   }
 
   console.log(formData);
